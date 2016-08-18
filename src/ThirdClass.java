7901f0f32989b7555310ae6b3c21775447dc6360
Tory <vikesha@mail.ru>
2016-08-18 11:27:02 +0200

    commit first homework

---
+++ src/ThirdClass.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Виктория on 18.08.2016.
+ */
+public class ThirdClass {
+    public static void main(String[] args) {
+        System.out.println("My first programs on Java...");
+    }
+}
